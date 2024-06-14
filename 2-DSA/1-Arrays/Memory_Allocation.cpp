3acc36d3b56e632291e6780fffd6cfe564403f10
Kishore-Nayak <kishoredeveloper123@gmail.com>
2024-06-14 10:48:54 +0530

    14-06-2024

--- 2-DSA/1-Arrays/Memory_Allocation.cpp
+++ 2-DSA/1-Arrays/Memory_Allocation.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main()
 {
     int arr[]={1,2,3,4};
-    cout<<&arr[0]<<endl;   //0x7ffc79455dc0             in gap og 4 bytes due to integer
+
+    cout<<&arr<<endl;      //0x7ffc79455dc0
+    cout<<arr<<endl;       //0x7ffc79455dc0
+    cout<<&arr[0]<<endl;   //0x7ffc79455dc0             in gap of 4 bytes due to integer
+
     cout<<&arr[1]<<endl;   //0x7ffc79455dc4
     cout<<&arr[2]<<endl;   //0x7ffc79455dc8
     cout<<&arr[3]<<endl;   //0x7ffc79455dcc
