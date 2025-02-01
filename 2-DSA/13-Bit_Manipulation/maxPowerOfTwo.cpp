394f0a111edc05c60ce03b915e150daa768cf14f
Kishore Nayak <kishoredeveloper123@gmail.com>
2025-02-01 17:14:20 +0530

    01-02-2025

--- 2-DSA/13-Bit_Manipulation/maxPowerOfTwo.cpp
+++ 2-DSA/13-Bit_Manipulation/maxPowerOfTwo.cpp
@@ -1,9 +1,16 @@
 //max power of 2 less than equal to n
 #include<iostream>
-#include<string>
-#include<cmath>
 using namespace std;
-long max_power_of_two(int n){
+
+int max_power_of_two_1(int n){
+    n = n | (n >> 1);
+    n = n | (n >> 2);
+    n = n | (n >> 4);
+    n = n | (n >> 8);
+    return (n+1)>>1;
+}
+
+int max_power_of_two_2(int n){
     long temp = n;
     while(n){
         temp = n;
@@ -12,6 +19,7 @@ long max_power_of_two(int n){
     return temp;
 }
 int main(){
-    cout<<max_power_of_two(1029)<<endl;
+    cout<<max_power_of_two_1(1029)<<endl;
+    cout<<max_power_of_two_2(1029)<<endl;
     return 0;
 }
--- 2-DSA/14-Sliding_Window/maximumSizeSubarrayOfSizeK.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[] = {7,1,2,5,8,4,9,3,6};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int k = 3;
-    
-    return 0;
-}
